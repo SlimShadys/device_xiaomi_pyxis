a8524c785cb8cf43efd5c61d1bd5da235967895a
Gianmarco Scarano <gianmarcoscarano@gmail.com>
2020-08-26 13:55:15 +0000

    pyxis: Adapt overlays for Arrow
    Signed-off-by: Gianmarco Scarano <gianmarcoscarano@gmail.com>

--- device.mk
+++ device.mk
@@ -31,8 +31,7 @@ EXTRA_FOD_ANIMATIONS := true
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
